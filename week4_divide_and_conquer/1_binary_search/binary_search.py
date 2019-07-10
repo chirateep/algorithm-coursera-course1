848394a5d65af4f27b149738dbab5c708e485d8a
chirateep <tabechi155@gmail.com>
2019-07-10 22:07:27 +0700

    update binary search to finish

--- week4_divide_and_conquer/1_binary_search/binary_search.py
+++ week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -1,9 +1,25 @@
 # Uses python3
 import sys
 
+
 def binary_search(a, x):
     left, right = 0, len(a)
-    # write your code here
+    if a[left] > x or a[len(a) - 1] < x:
+        return -1
+
+    while(left <= right):
+        mid = (left + right) // 2
+        # print('mid', mid)
+        if a[mid] == x:
+            return mid
+
+        if a[mid] < x:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -11,12 +27,13 @@ def linear_search(a, x):
             return i
     return -1
 
+
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n = data[0]
     m = data[n + 1]
-    a = data[1 : n + 1]
+    a = data[1: n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end=' ')
