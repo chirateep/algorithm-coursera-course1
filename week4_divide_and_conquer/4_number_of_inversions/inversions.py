e6ce184e7c0e2f410dab80df830d8f20d0cab632
chirateep <tabechi155@gmail.com>
2019-07-10 22:16:35 +0700

    update inversion to finish

--- week4_divide_and_conquer/4_number_of_inversions/inversions.py
+++ week4_divide_and_conquer/4_number_of_inversions/inversions.py
@@ -10,6 +10,23 @@ def get_number_of_inversions(a, b, left, right):
     number_of_inversions += get_number_of_inversions(a, b, left, ave)
     number_of_inversions += get_number_of_inversions(a, b, ave, right)
     # write your code here
+    left_half = a[left:ave]
+    right_half = a[ave:right]
+    index_result = left
+    while (len(left_half) > 0 and len(right_half) > 0):
+        if left_half[0] <= right_half[0]:
+            a[index_result] = left_half.pop(0)
+        else:
+            number_of_inversions += 1 * len(left_half)
+            a[index_result] = right_half.pop(0)
+        index_result += 1
+    if len(left_half) == 0:
+        a[index_result:right] = right_half
+    else:
+        a[index_result:right] = left_half
+
+    # print(a)
+    # print("num", number_of_inversions)
     return number_of_inversions
 
 
