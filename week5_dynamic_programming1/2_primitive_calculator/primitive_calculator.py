fb47a9c2d1272d50195c54924e4283a6549fa7a9
chirateep <tabechi155@gmail.com>
2019-07-11 16:36:22 +0700

    finish primitive calculator but big mem

--- week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -1,6 +1,7 @@
 # Uses python3
 import sys
 
+
 def optimal_sequence(n):
     sequence = []
     while n >= 1:
@@ -13,9 +14,41 @@ def optimal_sequence(n):
             n = n - 1
     return reversed(sequence)
 
+
+def dp_optimal_sequence(n):
+    min_num_opt = [n + 1] * (n + 1)
+    map_otp_seq = dict()
+    operations = ["+1", "*2", "*3"]
+
+    min_num_opt[1] = 0
+    map_otp_seq[1] = [1]
+
+    for i in range(2, n + 1):
+        for opt in operations:
+            opt_i = operate_i(i, opt)
+            min_opt = min_num_opt[opt_i] + 1
+            if min_opt < min_num_opt[i]:
+                min_num_opt[i] = min_opt
+                map_otp_seq[i] = map_otp_seq[opt_i][:]
+                map_otp_seq[i].append(i)
+        # print(min_num_opt)
+        # print(map_otp_seq)
+    # print(map_otp_seq)
+    return map_otp_seq[n]
+
+
+def operate_i(i, opt):
+    if opt == "*2" and i % 2 == 0:
+        return i // 2
+    elif opt == "*3" and i % 3 == 0:
+        return i // 3
+
+    return i - 1
+
+
 input = sys.stdin.read()
 n = int(input)
-sequence = list(optimal_sequence(n))
+sequence = list(dp_optimal_sequence(n))
 print(len(sequence) - 1)
 for x in sequence:
     print(x, end=' ')
